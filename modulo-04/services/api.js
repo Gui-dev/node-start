0ed9d6e62c9be1e062644969fef578da468001f7
Gui-dev <adrianon10@gmail.com>
2020-12-09 02:57:09 -0300

    tests: add test

--- modulo-04/services/api.js
+++ modulo-04/services/api.js
@@ -7,10 +7,18 @@ const api = axios.create({
 const getPeople = async (name) => {
   const { data } = await api.get(`/?search=${name}`)
 
-  return data
+  return data.results.map(getMapPeople)
+}
+
+const getMapPeople = async (item) => {
+  return {
+    nome: item.name,
+    peso: item.height
+  }
 }
 
 module.exports = {
   api,
-  getPeople
+  getPeople,
+  getMapPeople
 }

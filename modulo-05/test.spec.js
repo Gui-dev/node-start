f08813b08045b8c2c13d04d157b821fbdd9fab86
Gui-dev <adrianon10@gmail.com>
2020-12-10 03:25:14 -0300

    test: should delete a hero by id, using files

--- modulo-05/test.spec.js
+++ modulo-05/test.spec.js
@@ -7,18 +7,30 @@ const DEFAULT_ITEM_CADASTRAR = {
 }
 
 describe ('Suite de manipulação de Heróis', () => {
-  it ('deve pesquisar um herói, usando arquivos', async () => {
+
+  beforeAll(async () => {
+    await database.setRegisterHero (DEFAULT_ITEM_CADASTRAR)
+  })
+
+  it ('deveria pesquisar um herói, usando arquivos', async () => {
     const expected = DEFAULT_ITEM_CADASTRAR
-    const [result] = await database.list(expected.id)
+    const [result] = await database.list (expected.id)
 
     expect(expected).toEqual(result)
   })
 
-  it ('deve cadastrar um herói, usando arquivos', async () => {
+  it ('deveria cadastrar um herói, usando arquivos', async () => {
     const expected = DEFAULT_ITEM_CADASTRAR
-    const result = await database.setRegisterHero(DEFAULT_ITEM_CADASTRAR)
-    const [actual] = await database.list(DEFAULT_ITEM_CADASTRAR.id)
+    const result = await database.setRegisterHero (DEFAULT_ITEM_CADASTRAR)
+    const [actual] = await database.list (DEFAULT_ITEM_CADASTRAR.id)
 
     expect(expected).toEqual(actual)
+  })
+
+  it ('deveria deletar um herói por id, usando arquivos', async () => {
+    const expected = true
+    const result = await database.delete (DEFAULT_ITEM_CADASTRAR.id)
+
+    expect(expected).toEqual(result)
   })
 })
